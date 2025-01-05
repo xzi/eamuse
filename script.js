f379ffb456b51a9dc875126a1ee924897d856c13
xzi <96212687+xzi@users.noreply.github.com>
2025-01-05 09:11:29 -0500

    Add a basic timer

---
+++ script.js
@@ -0,0 +1,32 @@
+// Thanks W3Schools. God I am dumb
+
+// Set the date we're counting down to
+var dailyMaintenance = new Date("2025-01-06T05:00:00+09:00").getTime();
+//var dailyMaintenance = new Date("2025-01-01T05:00:00+09:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = dailyMaintenance - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="maintenance"
+  // We have the ability to show days here, I just removed it because we don't need it.
+  document.getElementById("maintenance").innerHTML = hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("maintenance").innerHTML = "Maintenance is now live";
+  }
+}, 1000);
